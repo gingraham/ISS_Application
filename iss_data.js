de9ae324df14ef861924794fb5c1a5e136018d84
Gerald Ingraham <geraldingraham@Geralds-MBP.attlocal.net>
2023-04-18 18:18:56 -0400

    fix | map render async with iss data response

--- iss_data.js
+++ iss_data.js
@@ -7,58 +7,56 @@ async function getISS(){
     const {latitude, longitude} = data;
     document.getElementById('lat').textContent = latitude;
     document.getElementById('long').textContent = longitude;
-mapboxgl.accessToken = 'pk.eyJ1IjoiZ2luZ3JhaGFtIiwiYSI6ImNsZ2xtYm9xYjFxcWszbW55aWFnaTlmdjMifQ.LXHyb2N3Aod4VM9UywV51w';
-
-const geojson = {
-    'type': 'FeatureCollection',
-    'features': [
+    mapboxgl.accessToken = 'pk.eyJ1IjoiZ2luZ3JhaGFtIiwiYSI6ImNsZ2xtYm9xYjFxcWszbW55aWFnaTlmdjMifQ.LXHyb2N3Aod4VM9UywV51w';
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+        style: 'mapbox://styles/mapbox/dark-v11', // style URL
+        zoom: 1.5, // starting zoom
+        center: [latitude, longitude] // starting position
+        });
+         
+        map.on('load', () => {
+        // Load an image from an external URL.
+        map.loadImage(
+        'international-space-station-63128x1280.png',
+        (error, image) => {
+        if (error) throw error;
+         
+        // Add the image to the map style.
+        map.addImage('cat', image);
+         
+        // Add a data source containing one point feature.
+        map.addSource('point', {
+        'type': 'geojson',
+        'data': {
+        'type': 'FeatureCollection',
+        'features': [
         {
-            'type': 'Feature',
-            'properties': {
-                'message': 'Foo',
-                'iconSize': [600, 600]
-            },
-            'geometry': {
-                'type': 'Point',
-                'coordinates': [latitude, longitude]
-            }
-        },            
-    ]
-};
-
-const map = new mapboxgl.Map({
-    container: 'map',
-    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-    style: 'mapbox://styles/mapbox/streets-v11',
-    center: [latitude, longitude],
-    zoom: 1.5,
-    projection: 'globe',
+        'type': 'Feature',
+        'geometry': {
+        'type': 'Point',
+        'coordinates': [latitude, longitude]
+        }
+        }
+        ]
+        }
+        });
+         
+        // Add a layer to use the image to represent the data.
+        map.addLayer({
+        'id': 'points',
+        'type': 'symbol',
+        'source': 'point', // reference the data source
+        'layout': {
+        'icon-image': 'cat', // reference the image
+        'icon-size': 0.25
+        }
+        });
+        }
+        );
+        });
   
-});
-
-// Add markers to the map.
-for (const marker of geojson.features) {
-    // Create a DOM element for each marker.
-    const el = document.createElement('div');
-    const width = marker.properties.iconSize[0];
-    const height = marker.properties.iconSize[1];
-    el.className = 'marker';
-    el.style.backgroundImage = `url(international-space-station-63128x1280.png)`;
-    el.style.width = `${width}px`;
-    el.style.height = `${height}px`;
-    el.style.backgroundSize = '100%';
-
-    el.addEventListener('click', () => {
-        window.alert(marker.properties.message);
-    });
-
-    // Add markers to the map.
-    new mapboxgl.Marker(el)
-        .setLngLat(marker.geometry.coordinates)
-        .addTo(map);
 }
 
-}
 getISS()
-
-
